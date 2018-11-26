fefb4dc6a68ce5e21bd78f46e7d80c2dc97bc12c
Megan Farley <megfarley4@gmail.com>
2018-11-26 17:57:52 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(POKEMON)
-  //YOUR CODE HERE
+
+  const pokemonSearchForm = document.querySelector('#pokemon-search-form')
+  const pokemonContainer = document.querySelector('#pokemon-container')
+  const pokemonSearchInput = document.querySelector('#pokemon-search-input')
+
+  const render = function(){
+
+    fetch('http://localhost:3000/pokemon')
+      .then( function(response) {
+        return response.json()
+      })
+      .then ( function(pokemon) {
+        renderPokemon(pokemon)
+        pokemonSearchInput.addEventListener('keyup', function(e){
+          let search = e.target.value
+          renderPokemon(pokemon, search)
+        })
+      })
+
+  };
+
+  const renderPokemon = function(pokemonList, filter = false){
+    pokemonContainer.innerHTML = ''
+    for(const pokemon of pokemonList){
+      if(!filter || pokemon.name.includes(filter)){
+        const pokemonDetail = document.createElement('div')
+        pokemonDetail.name = pokemon.name
+        pokemonDetail.style = "width:230px;margin:10px;background:#fecd2f;color:#2d72fc;display:inline-block"
+        pokemonContainer.append(pokemonDetail)
+
+        const pokemonName = document.createElement('h1')
+        pokemonName.innerHTML = pokemon.name
+        pokemonName.class = 'center-text'
+        pokemonDetail.append(pokemonName)
+
+        const pokemonImage = document.createElement('img')
+        var image_tracker = 'front';
+        pokemonImage.src = pokemon.sprites.front
+        pokemonImage.addEventListener('click', function(e){
+          if(image_tracker == 'front'){
+              pokemonImage.src = pokemon.sprites.back
+              image_tracker = 'back'
+            }else{
+              pokemonImage.src = pokemon.sprites.front
+              image_tracker = 'front';
+            }
+        })
+        pokemonDetail.append(pokemonImage)
+      }
+    };
+  };
+
+
+  render()
+
 })
